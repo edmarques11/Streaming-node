4178b6c88318d2a8a2baa861a0d4c6105e775607
Edmarques Lima <edmarquessantosdelima@gmail.com>
2022-01-14 23:37:57 -0300

    init nodejs course upload files

---
+++ infraestrutura/conexao.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql')
+
+console.log(process.env)
+
+const conexao = mysql.createConnection({
+    host: process.env.NODE_ENV_DATABASE_HOST,
+    port: process.env.NODE_ENV_DATABASE_PORT,
+    user: process.env.NODE_ENV_DATABASE_USER,
+    password: process.env.NODE_ENV_DATABASE_PASSWORD,
+    database: process.env.NODE_ENV_DATABASE_NAME
+})
+
+
+module.exports = conexao
